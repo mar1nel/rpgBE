43d422f4d4a739e14b5df50de8035a0b48b9bcc8
mar1nel <vicu-marin.ababii@student.upt.ro>
2024-02-28 23:33:15 +0200

    test

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ const mongoURI = 'mongodb://127.0.0.1:27017/rpgLicence'; // Your database name
 mongoose.connect(mongoURI)
     .then(() => console.log('Successfully connected to MongoDB'))
     .catch((err) => console.error('Error connecting to MongoDB:', err));
-//testtest
+//testtesttest
