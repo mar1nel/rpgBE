848a59644e9537fa2f9e048e884864b1326433d9
mar1nel <vicu-marin.ababii@student.upt.ro>
2024-06-01 20:51:56 +0300

    removed email & picture from profile

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -4,20 +4,16 @@ interface IProfile extends Document {
     profileNickname: string;
     passwordHash: string;
     solanaAddress: string;
-    profileEmail: string;
     class: string;
     money: number;
-    profilePicture: string;
 }
 
 const profileSchema: Schema = new Schema({
     profileNickname: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
     solanaAddress: { type: String, required: true, unique: true },
-    profileEmail: { type: String, required: true },
     class: { type: String, required: true },
     money: { type: Number, default: 0 },
-    profilePicture: { type: String, required: true }
 }, { timestamps: true });
 
 const Profile = mongoose.model<IProfile>('Profile', profileSchema);
