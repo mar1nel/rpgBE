6dfffa7f69df05ed17819341ff164489f6832ce4
mar1nel <vicu-marin.ababii@student.upt.ro>
2024-06-03 23:54:04 +0300

    added item id quantity imageurl

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -1,21 +1,29 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface IItem extends Document {
+    itemId: number;
     itemName: string;
     itemType: string;
     rarity: 'A' | 'B' | 'C' | 'F';
     price: number;
     sellDuration: number;
     bonusStats: mongoose.Schema.Types.Mixed;
+    stackable: boolean;
+    quantity: number;
+    imageUrl: string;
 }
 
 const itemSchema: Schema = new Schema({
-    itemName: { type: String, required: true },
+    itemId: { type: Number, required: true, unique: true },
+    itemName: { type: String, required: true, unique: true },
     itemType: { type: String, required: true },
     rarity: { type: String, required: true, enum: ['A', 'B', 'C', 'F'] },
     price: { type: Number, required: true },
     sellDuration: { type: Number, required: true },
-    bonusStats: { type: mongoose.Schema.Types.Mixed }
+    bonusStats: { type: mongoose.Schema.Types.Mixed },
+    stackable: { type: Boolean, default: false },
+    quantity: { type: Number, default: 1 }, // Number of items in the stack
+    imageUrl: { type: String, required: true }
 });
 
 const Item = mongoose.model<IItem>('Item', itemSchema);
--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -7,16 +7,27 @@ interface IProfile extends Document {
     money: number;
     level: number;
     healthPoints: number;
+    inventory: IInventoryItem[];
+}
+
+interface IInventoryItem {
+    itemId: number;  // Reference to Item.itemId
+    quantity: number;
+    equipped: boolean;
 }
 
 const profileSchema: Schema = new Schema({
     profileNickname: { type: String, required: true, unique: true },
     solanaAddress: { type: String, required: true, unique: true },
-    profileClass: { type: String, required: true },
+    profileClass: { type: Number, required: true },  // Adjusted to Number if matching itemId
     money: { type: Number, default: 100 },
     level: { type: Number, default: 0 },
     healthPoints: { type: Number, default: 100 },
-
+    inventory: [{
+        itemId: { type: Number, required: true },
+        quantity: { type: Number, required: true },
+        equipped: { type: Boolean, default: false }
+    }]
 }, { timestamps: true });
 
 const Profile = mongoose.model<IProfile>('Profile', profileSchema);
